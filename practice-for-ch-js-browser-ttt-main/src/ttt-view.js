6af51ce476f09f6505a9dcd3be237c820611c7b7
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-12 16:58:00 -0500

    finish it

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -10,7 +10,9 @@ class View {
     [0, 1, 2].forEach((i)=>{
       [0, 1, 2].forEach((j)=>{
         const li = document.createElement('li');
-        li.onclick = this.handleClick;
+        li.onclick = (e) => this.handleClick(e);
+        li.classList.add("text-white", "text-center", "text-8xl", "align-middle");
+        li.style.lineHeight = "215px";
         li.dataset.row = i;
         li.dataset.col = j;
         grid.appendChild(li);
@@ -21,8 +23,20 @@ class View {
   };
   
   handleClick(e) {
-    // console.log(e);
-    this.game.playMove([e.target.dataset]);
+    /**@type {HTMLLIElement} */
+    const boardSpace = e.target;
+    const pos = [boardSpace.dataset.row, boardSpace.dataset.col];
+    try {
+      this.game.playMove(pos);
+      const mark = this.game.board.grid[pos[0]][pos[1]];
+      boardSpace.classList.toggle("move");
+      boardSpace.innerText = mark;
+      const winner = this.game.winner();
+      if (!winner) return;
+      alert(`The winner is ${winner}!! Congratulations!`);
+    } catch {
+      alert('invalid!! bad >:( you used a space that was already occupied!!! :( bad bad bad');
+    }
   }
 
   makeMove(square) {
