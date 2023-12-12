48705da1ef91bd212ad92fa727b7a80c8a010705
Jacob Tabor <jtabor214@gmail.com>
2023-12-12 16:07:13 -0500

    Expanded on ttt-view.js, ttt.css, index.js, and index.html

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,13 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import View from "./ttt-view";
+import Game from "../ttt_node/game.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const boardView = document.querySelector('.ttt');
+  const newGame = new Game();
+  const newView = new View(newGame, boardView);
+
+  newView.setupBoard();
+ 
 });
+
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,11 +1,28 @@
 class View {
   constructor(game, el) {
+    this.game = game; 
+    this.el = el;
   }
   
   setupBoard() {
-  }
+    /** @type {HTMLUListElement} */
+    const grid = document.createElement('ul');
+    [0, 1, 2].forEach((i)=>{
+      [0, 1, 2].forEach((j)=>{
+        const li = document.createElement('li');
+        li.onclick = this.handleClick;
+        li.dataset.row = i;
+        li.dataset.col = j;
+        grid.appendChild(li);
+      });
+    });
+    this.el.appendChild(grid);
+    console.log(this.el, grid);
+  };
   
   handleClick(e) {
+    // console.log(e);
+    this.game.playMove([e.target.dataset]);
   }
 
   makeMove(square) {
